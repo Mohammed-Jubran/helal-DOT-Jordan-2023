7856e9cdcf029c300494d5c05138d163fe814a55
Mohammed-Jubran <mohammedjubran111@gmail.com>
2023-05-09 17:37:25 +0300

    Edit splash screen

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:helal/views/home_screen.dart';
+import 'package:helal/views/login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -8,14 +11,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    _checkLogin();
+  }
+
+  _checkLogin() async {
+    Future.delayed(const Duration(milliseconds: 3000), () async {
+      bool exists =
+          await const FlutterSecureStorage().containsKey(key: "token");
+
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                exists ? const HomeScreen() : const LoginScreen(),
+          ));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SafeArea(
         child: SizedBox(
           height: double.infinity,
-          width: double.infinity,
-          child: Image.asset(""),
+          child: Image.asset("assets/images/e-logo.PNG", fit: BoxFit.cover),
         ),
       ),
     );
