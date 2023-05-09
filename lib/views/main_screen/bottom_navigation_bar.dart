86e8909c15109762e735de71984deb271859706a
Mohammed-Jubran <mohammedjubran111@gmail.com>
2023-05-09 21:15:18 +0300

    edit many think

--- lib/views/main_screen/bottom_navigation_bar.dart
+++ lib/views/main_screen/bottom_navigation_bar.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:helal/views/about_us_screen.dart';
-import 'package:helal/views/cart_screen.dart';
-import 'package:helal/views/categories_screen.dart';
-import 'package:helal/views/home_screen.dart';
-import 'package:helal/views/more_screen.dart';
+import 'package:helal/views/main_screen/cart_screen.dart';
+import 'package:helal/views/main_screen/categories_screen.dart';
+import 'package:helal/views/main_screen/home_screen.dart';
+import 'package:helal/views/more_screen/more_screen.dart';
+
 import 'package:helal/views/search_screen.dart';
 
 class BottomNavigation extends StatefulWidget {
