08a1439fdf914675f3ab032f7fd2de47095de18f
Mohammed-Jubran <mohammedjubran111@gmail.com>
2023-05-09 18:24:02 +0300

    remove about as in Home page

--- lib/views/bottom_navigation_bar.dart
+++ lib/views/bottom_navigation_bar.dart
@@ -14,13 +14,12 @@ class BottomNavigation extends StatefulWidget {
 }
 
 class _BottomNavigationState extends State<BottomNavigation> {
-  int _selectedTab = 2;
+  int _selectedTab = 0;
 
   final List _pages = [
-    const AboutUsScreen(),
-    const CartScreen(),
     const HomeScreen(),
     const CategoriesScreen(),
+    const CartScreen(),
     const MoreScreen(),
   ];
 
@@ -78,12 +77,11 @@ class _BottomNavigationState extends State<BottomNavigation> {
           selectedItemColor: Colors.purple,
           unselectedItemColor: Colors.grey,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.info), label: "About Us"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart), label: "Cart"),
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
             BottomNavigationBarItem(
                 icon: Icon(Icons.category), label: "Categories"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart), label: "Cart"),
             BottomNavigationBarItem(
                 icon: Icon(Icons.more_horiz), label: "More"),
           ],
